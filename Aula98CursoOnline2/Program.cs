03f55f30f131152733d9ad1a323d1660e198f621
Jénifer Constantino <jeniferg00constantino@hotmail.com>
2020-08-18 20:02:13 +0100

    Projeto iniciado, mostrando a mensagem Ola Mundo e Modificado

---
+++ Aula98CursoOnline2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Aula98CursoOnline2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Modificar");
+        }
+    }
+}
